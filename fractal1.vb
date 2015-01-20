1b5837eda6113059436432a37d9cbf490da86339
Pavel Kretov <firegurafiku@gmail.com>
2015-01-20 17:51:42 +0300

    fractal1.vb: inline functions as subs
    Unfortunately, CST has a broken Visual Basic interpreter which mishandles
    functions and subroutines in macros. So there is no choice but to inline
    routines into main sub. Burn in hell, motherfuckers.
    
    This commit also does some refactoring about the names of variables. They
    became longer and more descriptive.
    
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- fractal1.vb
+++ fractal1.vb
@@ -1,40 +1,5 @@
 ' Fractal1
 
-Public Function CalcSegmentsNumber(ByVal N As Integer) As Integer
-	CalcSegmentsNumber = 4^(N-1)
-End Function
-
-Public Function SubdivideSegment(ByRef Xs() As Double, ByRef Ys() As Double, ByVal i As Integer) As Integer
-
-	Xs(i) = 1/3
-	Ys(i) = 0
-	i = i + 1
-
-	Xs(i) = 1/2
-	Ys(i) = Sqr(3)/6
-	i = i + 1
-
-	Xs(i) = 2/3
-	Ys(i) = 0
-	i = i + 1
-
-	SubdivideSegment = 3
-End Function
-
-Public Sub InsertArray(ByRef target() As Double, ByVal index As Integer, ByVal source() As Double, ByVal count As Integer)
-	Dim i As Integer
-	Dim N As Integer
-	N = UBound(target)
-	For i=N-count To index STEP -1
-		target(i+count) = target(i)
-	Next
-
-	For i=index To index+count-1
-		target(i) = source(i-index+1)
-	Next
-End Sub
-
-
 Option Explicit
 Sub Main ()
 
@@ -44,87 +9,99 @@ Sub Main ()
 	StoreDoubleParameter "fractal_width", 0.1
 	EndHide
 
-	Dim N As Integer
-	Dim L As Double
-	Dim D As Double
-	N = CInt(RestoreDoubleParameter("fractal_depth"))
-	L = RestoreDoubleParameter("fractal_length")
-	D = RestoreDoubleParameter("fractal_width")
+	Const MaxPoints = 10000
 
-	Dim Q As Integer
-	Dim P As Integer
-	Q = CalcSegmentsNumber(N)
-	P = Q + 1
+	Dim fractalDepth  As Integer
+	Dim fractalLength As Double
+	Dim fractalWidth  As Double
+	Dim pointsX(MaxPoints) As Double
+	Dim pointsY(MaxPoints) As Double
+	Dim subdivisionX(MaxPoints) As Double
+	Dim subdivisionY(MaxPoints) As Double
+	Dim subdivisionCount As Integer
+	Dim subdivisionPointIndex As Integer
+	Dim currentCount As Integer
+	Dim depthLevel As Integer
 
-	Dim Xs() As Double
-	Dim Ys() As Double
-	ReDim Xs(P)
-	ReDim Ys(P)
-	Xs(1) = 0
-	Ys(1) = 0
-	Xs(2) = L
-	Ys(2) = 0
+	Dim startPointX As Double
+	Dim startPointY As Double
+	Dim endPointX As Double
+	Dim endPointY As Double
+	Dim endPointIndex As Integer
+	Dim distance As Double
+	Dim cosF As Double
+	Dim sinF As Double
 
-	Dim TempXs() As Double
-	Dim TempYs() As Double
-	ReDim TempXs(P)
-	ReDim TempYs(P)
-
-	Dim TempCount As Integer
-	Dim level As Integer
 	Dim i As Integer
 	Dim j As Integer
-	Dim c As Integer
 	Dim x As Double
 	Dim y As Double
 	Dim xx As Double
 	Dim yy As Double
-	Dim x0 As Double
-	Dim y0 As Double
-	Dim x1 As Double
-	Dim y1 As Double
-	Dim dist As Double
-	Dim cosF As Double
-	Dim sinF As Double
 
+	'
+	fractalDepth  = CInt(RestoreDoubleParameter("fractal_depth"))
+	fractalLength = RestoreDoubleParameter("fractal_length")
+	fractalWidth  = RestoreDoubleParameter("fractal_width")
 
-	c = 2
-	For level = 0 To N
+	'
+	pointsX(1) = 0
+	pointsY(1) = 0
+	pointsX(2) = fractalLength
+	pointsY(2) = 0
 
-		For i=c To 2 STEP -1
+	currentCount = 2
+	For depthLevel = 1 To fractalDepth
 
-			x0 = Xs(i-1)
-			y0 = Ys(i-1)
-			x1 = Xs(i)
-			y1 = Ys(i)
-			dist = Sqr((x1-x0)^2 + (y1-y0)^2)
-			cosF = (x1-x0) / dist
-			sinF = (y1-y0) / dist
+		For endPointIndex=currentCount To 2 STEP -1
 
-			TempCount = SubdivideSegment(TempXs, TempYs, 1)
+			startPointX = pointsX(endPointIndex-1)
+			startPointY = pointsY(endPointIndex-1)
+			endPointX   = pointsX(endPointIndex)
+			endPointY   = pointsY(endPointIndex)
+			distance    = Sqr((endPointX-startPointX)^2 + (endPointY-startPointX)^2)
+			cosF        = (endPointX-startPointX) / distance
+			sinF        = (endPointY-startPointY) / distance
 
-			For j=1 To TempCount
-				x = TempXs(j)
-				y = TempYs(j)
+			'
+			subdivisionCount = 3
+			subdivisionX(1) = 1/3
+			subdivisionY(1) = 0
+			subdivisionX(2) = 1/2
+			subdivisionY(2) = Sqr(3)/6
+			subdivisionX(3) = 2/3
+			subdivisionY(3) = 0
 
-				x = x * dist
-				y = y * dist
+			For subdivisionPointIndex=1 To subdivisionCount
+				x = subdivisionX(subdivisionPointIndex)
+				y = subdivisionY(subdivisionPointIndex)
+
+				x = x * distance
+				y = y * distance
 
 				xx = x*cosF + y*sinF
 				yy = x*sinF - y*cosF
 
-				xx = xx + x0
-				yy = yy + y0
+				xx = xx + startPointX
+				yy = yy + startPointY
 
-				TempXs(j) = xx
-				TempYs(j) = yy
+				subdivisionX(subdivisionPointIndex) = xx
+				subdivisionY(subdivisionPointIndex) = yy
 			Next
 
-			InsertArray(Xs, i, TempXs, TempCount)
-			InsertArray(Ys, i, TempYs, TempCount)
-		Next
+			'
+			For i=MaxPoints-subdivisionCount To endPointIndex STEP -1
+				pointsX(i+subdivisionCount) = pointsX(i)
+				pointsY(i+subdivisionCount) = pointsY(i)
+			Next
 
-		c = c + TempCount
+			For i=endPointIndex To endPointIndex+subdivisionCount-1
+				pointsX(i) = subdivisionX(i-endPointIndex+1)
+				pointsY(i) = subdivisionY(i-endPointIndex+1)
+			Next
+
+			currentCount = currentCount + subdivisionCount
+		Next
 	Next
 
 	Curve.NewCurve "3D-Analytical"
@@ -133,11 +110,10 @@ Sub Main ()
 		.Name "fractal_curve"
 		.Curve "3D-Analytical"
 
-		For i = 0 To P
-			.Point Xs(i), Ys(i), 0
+		For i = 1 To currentCount
+			.Point pointsX(i), pointsY(i), 0
 		Next
 		.Create
 	End With
-
 End Sub
 
